ec197131e678deccbf68915e180bf6b81b64cc84
Shivendra-Katiyar-FSD <katiyar.shivendra82@gmail.com>
2022-12-19 22:51:53 +0530

    updates

--- Home/Slider/slider.js
+++ Home/Slider/slider.js
@@ -1,6 +1,6 @@
 function slider(){
     return `<div id="slider">
-    <img src="https://img-c.udemycdn.com/notices/home_banner/image_udlite/934d8b5e-6c87-4f1d-b483-b7cbb66eb218.png" alt="" id="start-image">
+    <img src="https://s.udemycdn.com/browse_components/billboard/fallback_banner_image_udlite.jpg" alt="" id="start-image">
 </div>`
 }
 export default slider;
@@ -13,6 +13,7 @@ let imgArray = ['https://s.udemycdn.com/browse_components/billboard/fallback_ban
 let i=0;
 let x = setInterval(function(){
     let slider = documet.getElementById('slider');
+    slider.innnerHTML=null;
     if(i===imgArray.length) i=0;
     let img = document.createElement('img');
     img.src = imgArray[i];
