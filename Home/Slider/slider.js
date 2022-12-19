348e47fd17075fcc66c33cf42bc7df80325da933
Shivendra-Katiyar-FSD <katiyar.shivendra82@gmail.com>
2022-12-20 02:09:54 +0530

    Updates

--- Home/Slider/slider.js
+++ Home/Slider/slider.js
@@ -7,6 +7,8 @@ function slider() {
 function autoSlide() {
   let imgArray = [
     "https://s.udemycdn.com/browse_components/billboard/fallback_banner_image_udlite.jpg",
+    "https://img-c.udemycdn.com/notices/featured_banner/image_udlite/5bf6274c-4a57-42ce-93d6-9775b06730be.jpg",
+    "https://img-c.udemycdn.com/notices/featured_banner/image_udlite/9ea59bc2-bd61-463e-9ce9-7e71e8e586ae.jpg",
     "https://img-c.udemycdn.com/notices/home_banner/image_udlite/934d8b5e-6c87-4f1d-b483-b7cbb66eb218.png",
   ];
 
@@ -21,7 +23,7 @@ function autoSlide() {
     slider.append(img);
     i++;
     console.log("img");
-  }, 3000);
+  }, 2000);
   
 }
 export { slider, autoSlide };
