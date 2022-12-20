56debc30fabf3f63c8ba00b90755ef74ba365069
Shivendra-Katiyar-FSD <katiyar.shivendra82@gmail.com>
2022-12-21 03:51:26 +0530

    Home Content

---
+++ Home/Home-Content/Finance/script.js
@@ -0,0 +1,321 @@
+function finance(){
+  return `<div id="fnc">
+  <div>
+    <h2>Top courses in Finance & Accounting</h2>
+  </div>
+  <div class="slide-container swiper">
+    <div class="slide-content">
+      <div class="card-wrapper swiper-wrapper">
+        <div class="card swiper-slide">
+          <a href="">
+            <div>
+              <img
+                src="https://img-c.udemycdn.com/course/240x135/2776760_f176_10.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>
+                100 Days of Code: The Complete Python Pro Bootcamp for 2023
+              </h4>
+              <span>Dr. Angela Yu</span>
+              <div class="rating">
+                <span>4.7</span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star fa-star-half-o"></span>
+                <span>(161,074)</span>
+              </div>
+              <span class="price">₹3,499</span>
+              <div>Bestseller</div>
+            </div>
+          </a>
+        </div>
+
+        <div class="card swiper-slide">
+          <a href="">
+            <div>
+              <img
+                src="https://images.ctfassets.net/aq13lwl6616q/5Vsjisy7BEI69JhQoonaZh/e3dad68c580259a55d3389cb897e5291/complete_web_devloper_zero_to_mastery.png?w=700&fm=webp"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>The Complete Web Developer in 2023: Zero to Mastery</h4>
+              <span>Andrei Neagoie</span>
+              <div class="rating">
+                <span>4.5</span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star fa-star-half-o"></span>
+                <span>(161,074)</span>
+              </div>
+              <span class="price">₹1599</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="card swiper-slide">
+          <a href="">
+            <div>
+              <img
+                src="https://img-c.udemycdn.com/course/240x135/2776760_f176_10.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>
+                100 Days of Code: The Complete Python Pro Bootcamp for 2023
+              </h4>
+              <span>Dr. Angela Yu</span>
+              <div class="rating">
+                <span>4.7</span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star fa-star-half-o"></span>
+                <span>(161,074)</span>
+              </div>
+              <span class="price">₹3,499</span>
+              <div>Bestseller</div>
+            </div>
+          </a>
+        </div>
+
+        <div class="card swiper-slide">
+          <a href="">
+            <div>
+              <img
+                src="https://images.ctfassets.net/aq13lwl6616q/5Vsjisy7BEI69JhQoonaZh/e3dad68c580259a55d3389cb897e5291/complete_web_devloper_zero_to_mastery.png?w=700&fm=webp"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>The Complete Web Developer in 2023: Zero to Mastery</h4>
+              <span>Andrei Neagoie</span>
+              <div class="rating">
+                <span>4.5</span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star fa-star-half-o"></span>
+                <span>(161,074)</span>
+              </div>
+              <span class="price">₹1599</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="card swiper-slide">
+          <a href="">
+            <div>
+              <img
+                src="https://img-c.udemycdn.com/course/240x135/2776760_f176_10.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>
+                100 Days of Code: The Complete Python Pro Bootcamp for 2023
+              </h4>
+              <span>Dr. Angela Yu</span>
+              <div class="rating">
+                <span>4.7</span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star fa-star-half-o"></span>
+                <span>(161,074)</span>
+              </div>
+              <span class="price">₹3,499</span>
+              <div>Bestseller</div>
+            </div>
+          </a>
+        </div>
+
+        <div class="card swiper-slide">
+          <a href="">
+            <div>
+              <img
+                src="https://images.ctfassets.net/aq13lwl6616q/5Vsjisy7BEI69JhQoonaZh/e3dad68c580259a55d3389cb897e5291/complete_web_devloper_zero_to_mastery.png?w=700&fm=webp"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>The Complete Web Developer in 2023: Zero to Mastery</h4>
+              <span>Andrei Neagoie</span>
+              <div class="rating">
+                <span>4.5</span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star fa-star-half-o"></span>
+                <span>(161,074)</span>
+              </div>
+              <span class="price">₹1599</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="card swiper-slide">
+          <a href="">
+            <div>
+              <img
+                src="https://img-c.udemycdn.com/course/240x135/2776760_f176_10.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>
+                100 Days of Code: The Complete Python Pro Bootcamp for 2023
+              </h4>
+              <span>Dr. Angela Yu</span>
+              <div class="rating">
+                <span>4.7</span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star fa-star-half-o"></span>
+                <span>(161,074)</span>
+              </div>
+              <span class="price">₹3,499</span>
+              <div>Bestseller</div>
+            </div>
+          </a>
+        </div>
+
+        <div class="card swiper-slide">
+          <a href="">
+            <div>
+              <img
+                src="https://images.ctfassets.net/aq13lwl6616q/5Vsjisy7BEI69JhQoonaZh/e3dad68c580259a55d3389cb897e5291/complete_web_devloper_zero_to_mastery.png?w=700&fm=webp"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>The Complete Web Developer in 2023: Zero to Mastery</h4>
+              <span>Andrei Neagoie</span>
+              <div class="rating">
+                <span>4.5</span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star fa-star-half-o"></span>
+                <span>(161,074)</span>
+              </div>
+              <span class="price">₹1599</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="card swiper-slide">
+          <a href="">
+            <div>
+              <img
+                src="https://img-c.udemycdn.com/course/240x135/2776760_f176_10.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>
+                100 Days of Code: The Complete Python Pro Bootcamp for 2023
+              </h4>
+              <span>Dr. Angela Yu</span>
+              <div class="rating">
+                <span>4.7</span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star fa-star-half-o"></span>
+                <span>(161,074)</span>
+              </div>
+              <span class="price">₹3,499</span>
+              <div>Bestseller</div>
+            </div>
+          </a>
+        </div>
+
+        <div class="card swiper-slide">
+            <a href="">
+              <div>
+                <img
+                  src="https://images.ctfassets.net/aq13lwl6616q/5Vsjisy7BEI69JhQoonaZh/e3dad68c580259a55d3389cb897e5291/complete_web_devloper_zero_to_mastery.png?w=700&fm=webp"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h4>The Complete Web Developer in 2023: Zero to Mastery</h4>
+                <span>Andrei Neagoie</span>
+                <div class="rating">
+                  <span>4.5</span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star fa-star-half-o"></span>
+                  <span>(161,074)</span>
+                </div>
+                <span class="price">₹1599</span>
+              </div>
+            </a>
+          </div>
+      </div>
+    </div>
+
+    <div class="swiper-button-next swiper-navBtn"></div>
+    <div class="swiper-button-prev swiper-navBtn"></div>
+  </div>
+</div>`
+}
+
+
+function swiper(){
+  var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+        1275: {
+          slidesPerView: 4,
+        },
+        1500: {
+          slidesPerView: 5,
+        }
+    },
+  });
+}
+
+export {finance, swiper};
