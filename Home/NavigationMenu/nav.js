3bf5a1275cd0af0b972142d4255d2b99e2bc1e9f
Shivendra-Katiyar-FSD <katiyar.shivendra82@gmail.com>
2022-12-21 01:35:22 +0530

    Navigation Updates

---
+++ Home/NavigationMenu/nav.js
@@ -0,0 +1,108 @@
+function navigationMenu(){
+    return `<div class="wrapper3">
+    <div class="right-menu3">
+      <a href=""><button class="menu-button3">Development</button></a>
+      <div class="dropdown-menu3">
+        <a href="">Web Development</a>
+        <a href="">Mobile Development</a>
+        <a href="">Programming Languages</a>
+        <a href="">Game Development</a>
+        <a href="">Database Development</a>
+      </div>
+    </div>
+
+    <div class="right-menu3">
+        <button class="menu-button3">Business</button>
+        <div class="dropdown-menu3">
+          <a href="">Enterpreneurship</a>
+          <a href="">Communication</a>
+          <a href="">Management</a>
+          <a href="">Sales</a>
+          <a href="">Business Strategy</a>
+        </div>
+    </div>
+
+    <div class="right-menu3">
+        <button class="menu-button3">Finance & Accounting</button>
+        <div class="dropdown-menu3">
+          <a href="">Cryptocurrency</a>
+          <a href="">Finance</a>
+          <a href="">Financial Modelling</a>
+          <a href="">Investing & Trading</a>
+        </div>
+    </div>
+
+    <div class="right-menu3">
+        <button class="menu-button3">IT & Software</button>
+        <div class="dropdown-menu3">
+          <a href="">IT Certifications</a>
+          <a href="">Network & Security</a>
+          <a href="">Hardware</a>
+          <a href="">OS & Servers</a>
+          <a href="">Other IT & Software</a>
+        </div>
+    </div>
+
+    <div class="right-menu3">
+        <button class="menu-button3">Office Productivity</button>
+        <div class="dropdown-menu3">
+            <a href="">Microsoft</a>
+            <a href="">Apple</a>
+            <a href="">Google</a>
+            <a href="">SAP</a>
+            <a href="">Oracle</a>
+        </div>
+    </div>
+
+    <div class="right-menu3">
+        <button class="menu-button3">Personal Development</button>
+        <div class="dropdown-menu3">
+            <a href="">Personal Transformation</a>
+            <a href="">Personal Productivity</a>
+            <a href="">Career Development</a>
+            <a href="">Parenting & Relationship</a>
+        </div>
+    </div>
+
+    <div class="right-menu3">
+        <button class="menu-button3">Design</button>
+        <div class="dropdown-menu3">
+            <a href="">Web Design</a>
+            <a href="">Graphic Design</a>
+            <a href="">User Interface</a>
+            <a href="">User Experience</a>
+        </div>
+    </div>
+
+    <div class="right-menu3">
+        <button class="menu-button3">Marketing</button>
+        <div class="dropdown-menu3">
+            <a href="">Digital Marketing</a>
+            <a href="">Network Marketing</a>
+            <a href="">Social Media</a>
+            <a href="">Branding</a>
+        </div>
+    </div>
+
+    <div class="right-menu3">
+        <button class="menu-button3">Health & Fitness</button>
+        <div class="dropdown-menu3">
+            <a href="">Fitness</a>
+            <a href="">General Health</a>
+            <a href="">Sports</a>
+            <a href="">Nutrition & Diet</a>
+        </div>
+    </div>
+
+    <div class="right-menu3">
+        <button class="menu-button3">Music</button>
+        <div class="dropdown-menu3">
+            <a href="">Instruments</a>
+            <a href="">Music</a>
+            <a href="">Techniques</a>
+        </div>
+    </div>
+</div>`
+}
+
+export default navigationMenu;
